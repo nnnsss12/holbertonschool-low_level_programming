00cb1b13aa8898a2d05c621d72a2b82bb7fc2561
nnnsss12 <ms.nora1424@gmail.com>
2026-02-23 18:55:44 +0000

    Add Task 9 - times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int result;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			result = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+					_putchar(' ');
+			}
+			_putchar(result / 10 + '0');
+			_putchar(result % 10 + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
