20184e877962c6a5baf311fd184bccdfb69fa523
nnnsss12 <ms.nora1424@gmail.com>
2026-03-02 17:49:47 +0000

    Add 3-print_numbers function

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
