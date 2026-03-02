48394d1270ee2eb7a48433cb2700ddd4b93893d1
nnnsss12 <ms.nora1424@gmail.com>
2026-03-02 18:23:51 +0000

    Fix Betty style: line length in task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,23 +12,21 @@ void puts_half(char *str)
 	int start;
 
 	len = 0;
-	/* حساب طول النص */
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 
-	/* تحديد نقطة البداية حسب القواعد المطلوبة */
 	if (len % 2 == 0)
 	{
 		start = len / 2;
 	}
 	else
 	{
+		/* n = (length_of_the_string + 1) / 2 */
 		start = (len + 1) / 2;
 	}
 
-	/* طباعة النصف الثاني */
 	while (str[start] != '\0')
 	{
 		_putchar(str[start]);
