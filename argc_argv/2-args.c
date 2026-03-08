a6afbdc98eb4e0f2281f14e7d7cc05bd81d745bc
nnnsss12 <ms.nora1424@gmail.com>
2026-03-08 19:05:49 +0000

    Add 2-args program

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments received
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
