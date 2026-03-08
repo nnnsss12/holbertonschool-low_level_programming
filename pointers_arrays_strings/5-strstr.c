c672e9342650999ade223f94b6e4c19a9bb64f63
nnnsss12 <ms.nora1424@gmail.com>
2026-03-08 18:22:41 +0000

    Add 5-strstr function

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to search in
+ * @needle: the substring to find
+ *
+ * Return: pointer to beginning of located substring, or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i;
+	unsigned int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && needle[j] != '\0')
+			j++;
+		if (needle[j] == '\0')
+			return (haystack + i);
+		i++;
+	}
+	return (NULL);
+}
